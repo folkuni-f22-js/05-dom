3b03808fc86a4457b773ccb511980de1531b0e8b
David Andersson <david.andersson@zocom.se>
2023-01-30 11:34:28 +0100

    On-off button example

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 console.log('Hello world')
 
+
+// Variables for DOM elements
 let h1 = document.querySelector('h1')
 let button = document.querySelector('button')
 let buttonClicks = document.querySelector('.button-clicks')
+let toggleButton = document.querySelector('#toggle-button')
 // console.log( 'h1:', h1 )
 
-let clicks = 0
 
+// State
+let state = {
+	clicks: 0,
+	isOn: true
+}
+
+
+// Events
 button.addEventListener('click', event => {
 	console.log('Du klickade på knappen')
 	// console.log(event)
-	clicks = clicks + 1
-	buttonClicks.innerText = 'Antal klick: ' + clicks
+	state.clicks = state.clicks + 1
+	buttonClicks.innerText = 'Antal klick: ' + state.clicks
 	
+})
+
+toggleButton.addEventListener('click', () => {
+	state.isOn = !state.isOn
+	if (state.isOn ) {
+		toggleButton.innerText = 'On'
+	} else {
+		toggleButton.innerText = 'Off'
+	}
 })
